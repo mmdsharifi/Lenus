e5a2fb1247f848b5c389f0e2869a3c99b696ce70
Mohammad sharifi <mmdsharifi@yahoo.com>
2015-09-06 15:26:08 +0430

    ADD : live reload plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,24 @@
 var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     notify = require('gulp-notify'),
+    connect = require('gulp-connect'),
     bower = require('gulp-bower');
 
 var config = {
-     sassPath: './resources/sass',
-     bowerDir: './bower_components'
+    sassPath: './resources/sass',
+    bowerDir: './bower_components',
+    htmlSources: '*.html'
 }
 
 gulp.task('bower', function() {
     return bower()
-      .pipe(gulp.dest(config.bowerDir))
+        .pipe(gulp.dest(config.bowerDir))
 });
 
 gulp.task('js', function() {
     return gulp.src([config.bowerDir + '/materialize/dist/js/materialize.min.js',
-                    config.bowerDir + '/jquery/dist/jquery.min.js'
-            ])
+            config.bowerDir + '/jquery/dist/jquery.min.js'
+        ])
         .pipe(gulp.dest('./public/js')); 
 });
 
@@ -27,21 +29,37 @@ gulp.task('icons', function() {
 
 gulp.task('css', function() {
     return sass(config.sassPath + '/style.scss', {
-      style: 'compressed',
-      loadPath: [
-        './resources/sass',
-        config.bowerDir + '/materialize/sass'
-      ]
-    })
+            style: 'compressed',
+            loadPath: [
+                './resources/sass',
+                config.bowerDir + '/materialize/sass'
+            ]
+        })
         .on('error', notify.onError(function(error) {
             return 'Error: ' + error.message;
         }))
-      .pipe(gulp.dest('./public/css'));
+        .pipe(gulp.dest('./public/css'))
+        .pipe(connect.reload());
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        root: './',
+        livereload: true
+    });
+});
+
+gulp.task('html', function() {
+    gulp.src(config.htmlSources)
+        .pipe(connect.reload())
 });
 
 // Rerun the task when a file changes
- gulp.task('watch', function() {
-     gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
+ 
+gulp.task('watch', function() {
+    gulp.watch(config.sassPath + '/**/*.scss', ['css']);
+    gulp.watch(config.htmlSources, ['html']);
 });
 
-  gulp.task('default', ['bower', 'icons', 'css','js']);
+  
+gulp.task('default', ['html', 'connect', 'watch', 'bower', 'icons', 'css', 'js']);
